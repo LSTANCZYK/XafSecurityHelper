7857f56213aab0b7d95930a33aa3d202f6576ea6
Александр <matlashallex32@gmail.com>
2016-03-17 18:12:43 +0300

    Init commit. 15.2.5 DX components version.

---
+++ XafSecurityHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XafSecurityHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XafSecurityHelper")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4067ac10-162d-4be2-b45c-021a5d56b4bf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ XafSecurityHelper/SecuirtyPermissionsFluent.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Security.Strategy;
+
+
+
+namespace XafSecurityHelper
+{
+    public static class SecurityPermissionsFluent
+    {
+
+        #region FluentMethods
+        private static string GetDescriptionAttribute(Enum value)
+        {
+
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+            else
+            {
+                throw new ArgumentException(String.Format("DescriptionAttribute not specified for SecurityOperationsEnum {0} value", value.ToString()));
+            }
+        }
+
+        private static List<Type> GetListTypesFromInterface(Type typeForInterfaces, Type typeForAssembly)
+        {
+            return Assembly.GetAssembly(typeForAssembly)
+              .GetExportedTypes()
+              .Where(t => t.IsClass && t.GetInterface(typeForInterfaces.FullName) != null)
+              .ToList();
+        }
+
+        public static SecuritySystemRole AddTypePermissionFluent<T>(this SecuritySystemRole securityRole, SecurityOperationsFluentExtension securityOperation)
+        {
+            if (typeof(T).IsInterface)
+            {
+                foreach (var type in GetListTypesFromInterface(typeof(T), securityRole.GetType()))
+                    securityRole.SetTypePermissions(type, GetDescriptionAttribute(securityOperation), DevExpress.ExpressApp.Security.Strategy.SecuritySystemModifier.Allow);
+
+                return securityRole;
+            }
+
+            securityRole.SetTypePermissions<T>(GetDescriptionAttribute(securityOperation), SecuritySystemModifier.Allow);
+            return securityRole;
+        }
+
+        public static SecuritySystemRole AddTypePermissionFluent(this SecuritySystemRole securityRole, List<Type> typeList, SecurityOperationsFluentExtension securityOperation)
+        {
+
+            foreach (var type in typeList)
+            {
+                if (type.IsInterface)
+                {
+                    foreach (var classType in GetListTypesFromInterface(type, securityRole.GetType()))
+                        securityRole.SetTypePermissions(classType, GetDescriptionAttribute(securityOperation), SecuritySystemModifier.Allow);
+                    continue;
+                }
+                securityRole.SetTypePermissions(type, GetDescriptionAttribute(securityOperation), SecuritySystemModifier.Allow);
+            }
+            return securityRole;
+        }
+
+        public static SecuritySystemRole AddMemberAccessPermissionFluent<T>(this SecuritySystemRole securityRole, string members, SecurityOperationsFluentExtension securityOperation, string criteria = null)
+        {
+
+            if (String.IsNullOrEmpty(members))
+                throw new ArgumentNullException(String.Format("Members arguments (Role {0} Type {1} ) is null or empty", securityRole.Name, typeof(T).FullName));
+
+            if (typeof(T).IsInterface)
+            {
+                foreach (var type in GetListTypesFromInterface(typeof(T), securityRole.GetType()))
+                {
+                    if (String.IsNullOrEmpty(criteria))
+                        securityRole.AddMemberAccessPermission(type, members, GetDescriptionAttribute(securityOperation));
+                    else
+                        securityRole.AddMemberAccessPermission(type, members, GetDescriptionAttribute(securityOperation), criteria);
+                }
+
+                return securityRole;
+            }
+
+            if (String.IsNullOrEmpty(criteria))
+                securityRole.AddMemberAccessPermission<T>(members, GetDescriptionAttribute(securityOperation));
+            else
+                securityRole.AddMemberAccessPermission<T>(members, GetDescriptionAttribute(securityOperation), criteria);
+
+            return securityRole;
+        }
+
+        public static SecuritySystemRole AddMemberAccessPermissionFluent(this SecuritySystemRole securityRole, List<Type> typeList, string members, SecurityOperationsFluentExtension securityOperation, string criteria = null)
+        {
+
+            if (String.IsNullOrEmpty(members))
+                throw new ArgumentNullException(String.Format("Members arguments (EmployeeRole {0} list types ) is null or empty", securityRole.Name));
+
+            foreach (var type in typeList)
+            {
+                if (type.IsInterface)
+                {
+                    foreach (var classType in GetListTypesFromInterface(type, securityRole.GetType()))
+                    {
+                        if (String.IsNullOrEmpty(criteria))
+                            securityRole.AddMemberAccessPermission(classType, members, GetDescriptionAttribute(securityOperation));
+                        else
+                            securityRole.AddMemberAccessPermission(classType, members, GetDescriptionAttribute(securityOperation), criteria);
+                    }
+                    continue;
+                }
+
+                if (String.IsNullOrEmpty(criteria))
+                    securityRole.AddMemberAccessPermission(type, members, GetDescriptionAttribute(securityOperation));
+                else
+                    securityRole.AddMemberAccessPermission(type, members, GetDescriptionAttribute(securityOperation), criteria);
+            }
+
+            return securityRole;
+        }
+        public static SecuritySystemRole AddObjectAccessPermissionFluent<T>(this SecuritySystemRole securityRole, SecurityOperationsFluentExtension securityOperation, string criteria)
+        {
+
+            if (String.IsNullOrEmpty(criteria))
+                throw new ArgumentNullException("SecurityOperations or criteria arguments is null or empty");
+
+            if (typeof(T).IsInterface)
+            {
+                foreach (var type in GetListTypesFromInterface(typeof(T), securityRole.GetType()))
+                    securityRole.AddObjectAccessPermission(type, criteria, GetDescriptionAttribute(securityOperation));
+
+                return securityRole;
+            }
+
+            securityRole.AddObjectAccessPermission<T>(criteria, GetDescriptionAttribute(securityOperation));
+            return securityRole;
+        }
+        #endregion
+    }
+
+    public enum SecurityOperationsFluentExtension
+    {
+        [DescriptionAttribute("Create")]
+        Create,
+        [DescriptionAttribute("Create;Read;Write;Delete")]
+        CRUDAccess,
+        [DescriptionAttribute("Delete")]
+        Delete,
+        [DescriptionAttribute("Read;Write;Delete;Navigate;Create")]
+        FullAccess,
+        [DescriptionAttribute("Read;Write;Delete;Navigate")]
+        FullObjectAccess,
+        [DescriptionAttribute("Navigate")]
+        Navigate,
+        [DescriptionAttribute("Read")]
+        Read,
+        [DescriptionAttribute("Read;Navigate")]
+        ReadOnlyAccess,
+        [DescriptionAttribute("Read;Write")]
+        ReadWriteAccess,
+        [DescriptionAttribute("Read;Write;Navigate")]
+        ReadWriteNavigate,
+        [DescriptionAttribute("Write")]
+        Write,
+        [DescriptionAttribute("Write;Create;Delete;Navigate")]
+        WriteCreateDeleteNavigate,
+        [DescriptionAttribute("Write;Create")]
+        WriteCreate,
+        [DescriptionAttribute("Write;Create;Delete")]
+        WriteCreateDelete,
+        [DescriptionAttribute("Write;Create;Navigate")]
+        WriteCreateNavigate,
+        [DescriptionAttribute("Write;Create;Read;Navigate")]
+        WriteCreateReadNavigate
+    }
+
+}
